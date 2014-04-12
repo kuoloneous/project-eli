f3aa9328b850132f75015b479269b9bb92ab75ba
kuoloneous <albert.kuo.123@gmail.com>
2014-04-12 13:50:12 -0700

    Updated target code

---
+++ Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSpawner : MonoBehaviour {
+
+	public GameObject targetObject;
+	public ArrayList targets;
+	// Use this for initialization
+	void Start () {
+		targets = new ArrayList();
+		targetObject.transform.position = RandomPosition ();
+		targets.Add (targetObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Check if target is hit
+		foreach (GameObject tar in targets) {
+			tar.GetComponent()
+			   }
+		if (targets.Count == 0) {
+			targetObject = Instantiate(targetObject, RandomPosition(), Quaternion.identity) as GameObject;
+			targets.Add(targetObject);
+			print ( "Target Position: " + targetObject.transform.position);
+			}
+	
+	}
+
+	Vector3 RandomPosition(){
+		Vector2 randomscreenpos = Camera.main.ScreenToWorldPoint(new Vector3 (Random.value, Random.value, 0));
+		Vector3 randompos = new Vector3(randomscreenpos.x, randomscreenpos.y, 0f);
+
+		return randompos;
+		}
+
+}
