783359db2f22d32e747ac8fec705f849662116cb
Mitsukizzy <ibenaven@usc.edu>
2014-04-11 23:57:55 -0700

    Initial Unity Project

---
+++ Eli Project/Assets/Scripts/player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public int score;
+	public int timer;
+	GUIText text_score;
+	GUIText text_timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		text_score.text = score.ToString();
+		text_timer.text = timer.ToString();
+	}
+}
