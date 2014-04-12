151eab7ffbdb6567524d03c49ff440be13ad9047
Mitsukizzy <ibenaven@usc.edu>
2014-04-12 11:12:25 -0700

    Updated the hit detection

--- Assets/Scripts/player_arcade.cs
+++ Assets/Scripts/player_arcade.cs
@@ -4,46 +4,57 @@ using System.Collections;
 public class player_arcade : MonoBehaviour {
 
 	public int score;
-	public int timer;
+	public int timer = 60;
 	public int time_left;
 	public float start_time;
 	public float time_past;
+	public bool sessionEnded;
 	public GUIText text_score;
 	public GUIText text_timer;
+	public GUIText text_high;
+	public GUIText text_session;
 
 	public Vector3 target_point; //where the ball hit
 	public Vector3 mouse; //testing mouse points
-	public GameObject point;
+
 
 	// Use this for initialization
 	void Start () {
 		start_time = Time.deltaTime; //get the current time
 		score = 0; //start with no points
-		timer = 60; //60 seconds
+		time_left = timer;
+		sessionEnded = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time_past = Time.time - start_time; //get time elapsed
-		time_left = Mathf.CeilToInt(timer - time_past); //make into an int 
-
 		//time is up
 		if (time_left <= 0) {
-			//do something
+			//set high score if current score beat previous scores
+			if(score > PlayerPrefs.GetInt("HighScore")){
+				PlayerPrefs.SetInt("HighScore" , score);
+			}
+			sessionEnded = true;
+		} else {
+			time_past = Time.time - start_time; //get time elapsed
+			time_left = Mathf.CeilToInt(timer - time_past); //make into an int 
 		}
 	}
 
 	void OnGUI(){
 		//change the live guitext objects to match the score and text variables
-		text_score.text = score.ToString();
+		text_score.text = "Score: " + score.ToString();
 		text_timer.text = "Seconds Left: " + time_left.ToString();
-	}
 
-	void OnMouseDown(){
-		Instantiate (point, Input.mousePosition, Quaternion.identity);
+		if (sessionEnded == true) {
+			text_high.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
+			text_session.text = "Session Score: " + score.ToString();
+		}
 	}
 
 	public void increaseScore(int pointsToAdd){
-		score += pointsToAdd;
+		if (!sessionEnded) {
+			score += pointsToAdd;
+		}
 	}
 }
