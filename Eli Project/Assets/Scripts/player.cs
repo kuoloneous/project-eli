73664b80e907172e4f17a3b8b99a001fa6ac2808
Mitsukizzy <ibenaven@usc.edu>
2014-04-12 00:31:01 -0700

    Unity Second Push

--- Eli Project/Assets/Scripts/player.cs
+++ Eli Project/Assets/Scripts/player.cs
@@ -5,21 +5,37 @@ public class player : MonoBehaviour {
 
 	public int score;
 	public int timer;
-	GUIText text_score;
-	GUIText text_timer;
+	public int time_left;
+	public float start_time;
+	public float time_past;
+	public GUIText text_score;
+	public GUIText text_timer;
 
 	// Use this for initialization
 	void Start () {
-	
+		start_time = Time.deltaTime; //get the current time
+		score = 0; //start with no points
+		timer = 60; //60 seconds
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		time_past = Time.time - start_time; //get time elapsed
+		time_left = Mathf.CeilToInt(timer - time_past); //make into an int 
+
+		//time is up
+		if (time_left <= 0) {
+			//do something
+		}
 	}
 
 	void OnGUI(){
+		//change the live guitext objects to match the score and text variables
 		text_score.text = score.ToString();
-		text_timer.text = timer.ToString();
+		text_timer.text = time_left.ToString();
+	}
+
+	void increaseScore(){
+
 	}
 }
